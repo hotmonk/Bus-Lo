419262ff3a7436b0d01a2cd116344b7c6ed93496
saurabhguptarock <saurabhguptarock119@gmail.com>
2019-10-06 21:22:48 +0530

    Added data retrival

--- slidenav.js
+++ slidenav.js
@@ -1,14 +1,7 @@
-/* Slidenav 4.0.0-alpha.5 ( https://github.com/tightcode/slidenav ) */
 (function($) {
-  "use strict";
-  // Slide effect animation breakpoint
-  // sm = 575.98px, md = 767.98px, lg = 991.98px or xl = 1199.98px
   var MaxWidth = window.matchMedia("(max-width: 767.98px)");
-  // Slide effect animation transition speed
   var slideSpeedAnim = 350;
-  // CSS media feature
   var mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce)");
-  // Slide effect animation toggle
   var slideSpeed;
   var slideSpeedInit = function() {
     if (mediaQuery.matches) {
@@ -23,30 +16,106 @@
   } else if (mediaQuery.attachEvent) {
     mediaQuery.attachEvent("change", slideSpeedInit);
   }
-  // Jquery slide effect
   $(".jq-slide").on("show.bs.dropdown", function() {
-    $(this).find(".dropdown-menu").first().stop(true, true).slideDown(slideSpeed);
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .slideDown(slideSpeed);
   });
   $(".jq-slide").on("hide.bs.dropdown", function() {
-    $(this).find(".dropdown-menu").first().stop(true, true).slideUp(slideSpeed);
+    $(this)
+      .find(".dropdown-menu")
+      .first()
+      .stop(true, true)
+      .slideUp(slideSpeed);
   });
-  // JqueryUI slide effect
   $(".jqui-slide").on("show.bs.dropdown", function() {
     if (MaxWidth.matches) {
-      $(this).find(".dropdown-menu").first().stop(true, true).slideDown(slideSpeed);
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .slideDown(slideSpeed);
     } else {
-      $(this).find(".dropdown-menu").first().stop(true, true).show("slide", {
-        direction: "up"
-      }, slideSpeed);
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .show(
+          "slide",
+          {
+            direction: "up"
+          },
+          slideSpeed
+        );
     }
   });
   $(".jqui-slide").on("hide.bs.dropdown", function() {
     if (MaxWidth.matches) {
-      $(this).find(".dropdown-menu").first().stop(true, true).slideUp(slideSpeed);
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .slideUp(slideSpeed);
     } else {
-      $(this).find(".dropdown-menu").first().stop(true, true).hide("slide", {
-        direction: "up"
-      }, slideSpeed);
+      $(this)
+        .find(".dropdown-menu")
+        .first()
+        .stop(true, true)
+        .hide(
+          "slide",
+          {
+            direction: "up"
+          },
+          slideSpeed
+        );
     }
   });
-}(jQuery));
+  firebase
+    .database()
+    .ref("/Bus")
+    .once("value")
+    .then(function(snap) {
+      data = snap.val();
+      document.getElementById("title").innerHTML = "Bus no :- " + 111;
+      document.getElementById("avgtime").innerHTML =
+        "Average time :- " + data[111].avgtime;
+
+      document.getElementById("cap").innerHTML = "Capacity :- " + data[111].cap;
+      document.getElementById("lstart").innerHTML =
+        "Journey Started :- " + data[111].lstart;
+      document.getElementById("occ").innerHTML =
+        "Occupancy :- " + data[111].occ;
+      document.getElementById("routedistance").innerHTML =
+        "Routed Distance :- " + data[111].routedistance;
+      var stops = data[111].stops;
+      datas = "";
+      stops.split("").forEach(d => (datas = datas.concat(d, " -> ")));
+      datas = datas.substring(0, datas.length - 3);
+      document.getElementById("stops").innerHTML = "Stops :- " + datas;
+    });
+})(jQuery);
+function showBus(no) {
+  firebase
+    .database()
+    .ref("/Bus")
+    .once("value")
+    .then(function(snap) {
+      data = snap.val();
+      document.getElementById("title").innerHTML = "Bus no :- " + no;
+      document.getElementById("avgtime").innerHTML =
+        "Average time :- " + data[no].avgtime;
+      document.getElementById("cap").innerHTML = "Capacity :- " + data[no].cap;
+      document.getElementById("lstart").innerHTML =
+        "Journey Started :- " + data[no].lstart;
+      document.getElementById("occ").innerHTML = "Occupancy :- " + data[no].occ;
+      document.getElementById("routedistance").innerHTML =
+        "Routed Distance :- " + data[no].routedistance;
+      var stops = data[no].stops;
+      datas = "";
+      stops.split("").forEach(d => (datas = datas.concat(d, " -> ")));
+      datas = datas.substring(0, datas.length - 3);
+      document.getElementById("stops").innerHTML = "Stops :- " + datas;
+    });
+}
